b2d1642e4cab30eb02381216027789b053430cbc
moududulhaque <moududulhaque@gmail.com>
2024-03-05 08:35:13 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return <div>
+    <h1>I am learning React</h1>
+    <input type="text" placeholder="My Name" />
+    <button>Submit</button>
+  </div>;
+};
+
+export default App;
